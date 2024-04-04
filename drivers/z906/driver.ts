ba651edf1464505426811385a05f4a128aaeefc2
Jordy Thien <jordy@thien.nu>
2024-04-04 23:18:14 +0200

    feat: Add send command flow

--- drivers/z906/driver.ts
+++ drivers/z906/driver.ts
@@ -4,8 +4,51 @@ import Z906Signal = require('../../lib/Z906Signal');
 
 const { RFDriver } = require('homey-rfdriver');
 
-module.exports = class Z906Driver extends RFDriver {
+class Z906Driver extends RFDriver {
 
   static SIGNAL = Z906Signal;
 
-};
+  async sendCommand({ commandId }: { commandId: string }) {
+    const signal = await this.getRFSignal();
+    await signal.cmd(commandId);
+  }
+
+  async onRFInit() {
+    await super.onRFInit();
+
+    this.homey.flow.getActionCard(`${this.id}:send_cmd`)
+      .registerRunListener(this.runListener())
+      .getArgument('cmd')
+      .registerAutocompleteListener(this.autocompleteListener());
+  }
+
+  private runListener() {
+    return async ({
+      device,
+      cmd,
+    }: { device: Z906Driver, cmd: { cmd: string } }) => {
+      return this.sendCommand({ commandId: cmd.cmd });
+    };
+  }
+
+  private autocompleteListener() {
+    return async (query: string) => {
+      const signal = await this.getRFSignal();
+
+      return Object.keys(signal.manifest.cmds)
+        .map((commandId) => {
+          return {
+            name: this.homey.__(`commands.${commandId}`) || commandId,
+            cmd: commandId,
+          };
+        })
+        .filter((command) => {
+          return command.name.toLowerCase()
+            .includes(query.toLowerCase());
+        });
+    };
+  }
+
+}
+
+module.exports = Z906Driver;
